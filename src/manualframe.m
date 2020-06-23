d2812b626d6b6c12ed91397adefe8afead3ba4a4
Kegomoditswe <43317468+Kegomoditswe@users.noreply.github.com>
2020-06-23 08:45:03 +0200

    Update manualframe.m

--- src/manualframe.m
+++ src/manualframe.m
@@ -1,4 +1,4 @@
-function manualFrame = manualFrame(data)
+function [manualFrame, soundEnvelope, manual_index] = manualFrame(data)
 
     %remove dc component
     data = data - mean(data);
@@ -6,17 +6,22 @@ function manualFrame = manualFrame(data)
     %normalize data 
     data = data / abs(max(data));
     
-    %get upper envelope
+    %get upper envelope. Assuming that the sound is symmetrical about the x
+    %axis
     
     [U,L] = envelope(data);
+    soundEnvelope = U;
     
-    %m = mean(U)
-    mph = mean(U)/3;
+    % calculate Min peak height
+    mph = mean(U)/2;
     
     % find the peaks
     [pks,ind] = findpeaks(U, 'MinPeakHeight', mph);
     
+    %given index of sound 
+    manual_index = ind;
+    
     manualFrame = zeros(length(data),1);
     manualFrame(ind) = 1;
-    
+ 
 end
