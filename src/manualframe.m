27b45e656f43a338805d20bcc60af69a8d71f9d0
Kegomoditswe <43317468+Kegomoditswe@users.noreply.github.com>
2020-06-23 12:02:40 +0200

    Update manualframe.m

--- src/manualframe.m
+++ src/manualframe.m
@@ -1,5 +1,9 @@
 function [manualFrame, soundEnvelope, manual_index] = manualFrame(data)
-
+    %Outputs:
+    % the upper envelope of a signal
+    % frame around voice 
+    % also outputs the indices where of the voice segments
+    
     %remove dc component
     data = data - mean(data);
     
@@ -13,15 +17,21 @@ function [manualFrame, soundEnvelope, manual_index] = manualFrame(data)
     soundEnvelope = U;
     
     % calculate Min peak height
+    
+    %TODO find an optimum mph that will work for noisy signals
     mph = mean(U)/2;
     
+    % find all the peaks
+    [pks2,ind2] = findpeaks(U);
+    testmph = mean(pks2);
+    
     % find the peaks
-    [pks,ind] = findpeaks(U, 'MinPeakHeight', mph);
+    [pks,ind] = findpeaks(U, 'MinPeakHeight', testmph);
     
     %  index of sound 
     manual_index = ind;
     
     manualFrame = zeros(length(data),1);
     manualFrame(ind) = 1;
- 
+    
 end
